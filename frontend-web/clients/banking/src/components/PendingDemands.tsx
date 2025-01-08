d50e8a1e589b47d5a8ed029e09bff848bbeee8aa
erik.garcia <erik.g.h2@gmail.com>
2025-01-08 12:56:14 +0100

    add sample component

---
+++ frontend-web/clients/banking/src/components/PendingDemands.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type PendingDemandsProps = {}
+
+export const PendingDemands = (props: PendingDemandsProps) => {
+    return (
+        <div>
+            TESTCITO TESTCITO
+        </div>
+    );
+};
+
+export default PendingDemands;
